71b037b19c057492b223d607cfc4e653fcb7dcb0
VishnuV <palebluedotlabs@gmail.com>
2024-01-19 08:07:15 +0530

    Updates

--- src/redux/ReduxProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { Provider } from "react-redux";
-import React from "react";
-import { store } from "./Store";
-
-export function ReduxProvider ({children}:{children:React.ReactNode}){
-    return <Provider store={store}>{children}</Provider>
-}
